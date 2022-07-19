b6e90291a4375864359379bae7473f3d2a260b1f
Edward Suwirya <edward_suwirya@yahoo.com>
2022-07-19 14:58:44 +0700

    Add error log

--- src/shared/apiClientFactory.js
+++ src/shared/apiClientFactory.js
@@ -1,18 +1,32 @@
 const ApiClientFactory = (client) => {
 
-    const doPost = async ({url = '', body = null}) => {
+    const doPost = async ({url = '', data = null}) => {
         try {
-            const response = await client({url, body});
+            const response = await client({url: url, method: 'post', data: data});
             return response.data;
         } catch (e) {
+            if (e.response) {
+                console.log("response error")
+            } else if (e.request) {
+                console.log("request error")
+            } else {
+                console.log('Error', e.message);
+            }
             throw e;
         }
     }
     const doGet = async ({url = ''}) => {
         try {
-            const response = await client({url});
+            const response = await client({url: url, method: 'get'});
             return response.data;
         } catch (e) {
+            if (e.response) {
+                console.log("response error")
+            } else if (e.request) {
+                console.log("request error")
+            } else {
+                console.log('Error', e.message);
+            }
             throw e;
         }
     }
