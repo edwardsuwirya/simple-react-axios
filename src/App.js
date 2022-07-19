c2c2ae8ebbcf020fbac4174325beb924d0b26947
Edward Suwirya <edward_suwirya@yahoo.com>
2022-07-19 11:58:20 +0700

    First Setup with Axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+// npm install axios --save
+
+const baseURL = "https://jsonplaceholder.typicode.com/posts";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [post, setPost] = useState(null);
+
+    useEffect(() => {
+        axios.get(baseURL + '/1').then((response) => {
+            setPost(response.data);
+        });
+    }, []);
+
+    function createPost() {
+        axios
+            .post(baseURL, {
+                title: "Hello World!",
+                body: "This is a new post."
+            })
+            .then((response) => {
+                setPost(response.data);
+            });
+    }
+
+    if (!post) return null;
+    return post ? (
+        <div>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+            <button onClick={createPost}>Create Post</button>
+        </div>
+    ) : (
+        <div></div>
+    )
 }
 
 export default App;
